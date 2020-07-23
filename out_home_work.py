b47436569c416ef9349ec52bcfd4003d25661966
alexandrmotologa <al.motologa@gmail.com>
2020-07-23 23:24:53 +0300

    scris codul

---
+++ out_home_work.py
@@ -0,0 +1,12 @@
+price_pizza_div = 75
+name_pizza_div = "Pizza Diverso"
+price_pizza_buf = 99
+name_pizza_buf = "Pizza Buffalo"
+price_pizza_chgo = 56
+name_pizza_chgo = "Pizza Chicago"
+
+print("\u2605"*20)
+print("1. ", name_pizza_div, ": ", price_pizza_div, "MDL")
+print("2. ", name_pizza_buf, ": ", price_pizza_buf, "MDL")
+print("3. ", name_pizza_chgo, ": ", price_pizza_chgo, "MDL")
+print("\u2605"*20)
